5221061443366e7596bd7df07877a51a40e267e1
CoDingDongHyuk <48036767+DongHyukki@users.noreply.github.com>
2023-07-06 21:58:31 +0900

    Revert "refactor : [ISSUE-4주차코드리뷰] (#6)"
    This reverts commit 21a61f78e7f379f1406729fdf44810dc81333663.

---
+++ src/main/java/com/zerobase/task/invite/domain/member/service/MemberServiceImpl.java
@@ -0,0 +1,23 @@
+package com.zerobase.task.invite.domain.member.service;
+
+import com.zerobase.task.invite.global.error.exception.BusinessException;
+import com.zerobase.task.invite.api.common.model.constant.ErrorCode;
+import com.zerobase.task.invite.domain.member.persistence.MemberRepository;
+import com.zerobase.task.invite.domain.member.persistence.entity.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public Member getMember(Long member_id) {
+        return memberRepository.findById(member_id)
+            .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND));
+    }
+}
